05d76b19f6d9b0e82cdcbdd3b2fe97eb8d4fc6df
shivbanarasi <shivbanarasi0542@gmail.com>
2023-04-12 13:43:37 +0530

    tyscript

---
+++ index.ts
@@ -0,0 +1,45 @@
+const num1Element=document.getElementById('num1') as HTMLInputElement
+const num2Element=document.getElementById("num2") as HTMLInputElement
+const buttonElement=document.querySelector('button')!;
+
+const numResult:number[]=[];
+const stringResult:string[]=[];
+
+// function add(num1:any,num2:any){
+// return num1+num2
+// }
+
+type numOrString=number|string  //example of type alias
+
+interface ResObj{                 //example of interface
+    val:number;
+    timestamp:Date 
+}
+
+function add(num1:numOrString,num2:numOrString){
+    if(typeof num1==="number"&&typeof num2==="number"){
+        return num1+num2;
+    }else if(typeof num1==="string"&&typeof num2==="string"){
+        return num1+" "+num2;
+    }
+}
+
+function printResult(resultObj:ResObj){
+    console.log(resultObj.val)
+}
+
+
+buttonElement.addEventListener('click',()=>{
+    const num1=num1Element.value;
+    const num2=num2Element.value;
+    const result=add(+num1,+num2)
+    const toStringres=add(num1,num2);
+    numResult.push(result as number);
+    stringResult.push(toStringres as string);
+    document.getElementById('print')!.innerHTML =`<p >number add: ${result.toString()} , and string add: ${toStringres}</p>` 
+    console.log(result)
+    printResult({val:result as number,timestamp:new Date})
+    console.log(numResult,stringResult)
+})
+ 
+
